3a74dccd8c02d0448d565e070e058a097adabe79
Gustavo Vieira <gustavovieira292020@gmail.com>
2022-10-01 09:32:18 -0300

    Create PythonE-modulo1.py

---
+++ PythonE-modulo1.py
@@ -0,0 +1,15 @@
+"""Neste módulo 1, aprenderá sobre:
+    -> Os fundamentos da programação informática, ou seja,
+    como o computador funciona, como o programa é executado, como a linguagem
+    de programação é definida e construída;
+    -> A diferença entre compilação e interpretação;
+    -> O que é o Python, como se posiciona entre outras linguagens de programação,
+    e o que distingue as diferentes versões de Python.
+
+
+    Um programa torna um computador utilizavel. Sem um programa, um computador,
+    mesmo o mais poderoso, nada mais é do que um objeto. Da mesma forma, sem um pianista,
+    um piano não é mais do que uma caixa de madeira.
+
+    
+"""
